6d873661b78a41ae808266698f9b06e86eb4e934
Alvaro Torres <61460887+Torres1373@users.noreply.github.com>
2023-12-04 23:55:46 +0100

    add new inputs

--- Characters/Player/FiniteStateMachine.gd
+++ Characters/Player/FiniteStateMachine.gd
@@ -1,8 +1,11 @@
 extends FiniteStateMachine
 
+#TODO JUMP
+
 func _init() -> void:
 	_add_state("idle")
 	_add_state("move")
+	_add_state("jump")
 
 func _ready() -> void:
 	set_state(states.idle)
@@ -16,15 +19,25 @@ func _get_transition() -> int:
 		states.idle:
 			if parent.velocity.length() > 10:
 				return states.move
-				
+			#if Input.is_action_pressed("ui_jump") and parent.is_on_floor():
+				#return states.jump
 		states.move:
 			if parent.velocity.length() < 10:
 				return states.idle
+			#if Input.is_action_pressed("ui_jump") and parent.is_on_floor():
+				#return states.jump
+		#states.jump:
+			#if parent.velocity.length() < 10 and parent.is_on_floor():
+				#return states.idle
+			#elif parent.velocity.length() > 10 and parent.is_on_floor():
+				#return states.move
 	return -1
-	
+
 func _enter_state(_previous_state: int, _new_state: int) -> void:
 	match _new_state:
 		states.idle:
 			animation_player.play("idle")
 		states.move:
 			animation_player.play("move")
+		states.jump:
+			animation_player.play("jump")
--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -2,6 +2,7 @@ extends Character
 
 func _process(_delta: float) -> void:
 	var mous_dir = Vector2(get_global_mouse_position() - global_position).normalized()
+	velocity.y += GRAVITY
 	
 	if mous_dir.x > 0 and animated_sprite.flip_h:
 		animated_sprite.flip_h = false
@@ -12,3 +13,4 @@ func get_input() -> void:
 	mov_dir = Vector2.ZERO
 	mov_dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	mov_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
