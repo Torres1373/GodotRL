31fd2b96fe0dd7769afb7736eb8bb30d7a9ea6c9
Alvaro Torres <61460887+Torres1373@users.noreply.github.com>
2023-12-04 22:22:13 +0100

    player movement

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -4,7 +4,10 @@ class_name Character
 const FRICTION: float = 0.15
 
 @export var accel: int = 40
-@export var maxVel: int = 100
+@export var max_speed: int = 100
+
+@onready var state_machine: Node = get_node("FiniteStateMachine")
+@onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
 
 var mov_dir: Vector2 = Vector2.ZERO
 
@@ -15,3 +18,4 @@ func _physics_process(_delta_: float) -> void:
 func move() -> void:
 	mov_dir= mov_dir.normalized()
 	velocity += mov_dir*accel
+	velocity = velocity.limit_length(max_speed)
---
+++ Characters/Player/player.gd
@@ -0,0 +1,14 @@
+extends Character
+
+func _process(_delta: float) -> void:
+	var mous_dir = Vector2(get_global_mouse_position() - global_position).normalized()
+	
+	if mous_dir.x > 0 and animated_sprite.flip_h:
+		animated_sprite.flip_h = false
+	elif mous_dir.x < 0 and not animated_sprite.flip_h:
+		animated_sprite.flip_h = true
+	
+func get_input() -> void:
+	mov_dir = Vector2.ZERO
+	mov_dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	mov_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
